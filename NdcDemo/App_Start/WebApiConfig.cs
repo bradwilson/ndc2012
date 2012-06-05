7c8437550672a948e89ab85f3847481f66b3c6a5
bradwilson <dotnetguy@gmail.com>
2012-06-05 23:18:39 +0200

    Step 3

---
+++ NdcDemo/App_Start/WebApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace NdcDemo
+{
+    public static class WebApiConfig
+    {
+        public static void Initialize(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                "apiRoute",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ NdcDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NdcDemo.Global" Language="C#" %>
